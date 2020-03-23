88ef4d8a041da57d32201f10d608656f1c72fe45
Josh <jharris4491@gmail.com>
2020-03-22 23:36:51 -0700

    updating fileservers to use env path

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,8 @@ FROM golang
 
 RUN go get -u github.com/tmpest/go-webserver
 
-# Enable module management
-# May or may not be necessary 
-# ENV GO111MODULE=on
-# ENV GOFLAGS=-mod=vendor
-
-# COPY /go/src/github.com/tmpest/go-webserver /go/src/github.com/tmpest/go-webserver
-
-# RUN go build go-webserver/cmd/webserver/webserver.go
 ENV WEBSERVER_ROOT "/go/src/github.com/tmpest/go-webserver"
-# ENTRYPOINT /go/bin/go-webserver
-ENTRYPOINT ping google.com
-# EXPOSE 8080gi
+
+ENTRYPOINT /go/bin/go-webserver
+
+EXPOSE 8080
--- internal/webserver/webserver.go
+++ internal/webserver/webserver.go
@@ -9,8 +9,8 @@ import (
 func Start() {
 	http.HandleFunc("/", viewHandler)
 	http.HandleFunc("/contact_us/recaptcha", recaptchaHandler)
-	http.Handle("/public/assets/images/", http.FileServer(http.Dir("")))
-	http.Handle("/public/javascript/", http.FileServer(http.Dir("")))
-	http.Handle("/public/stylesheets/", http.FileServer(http.Dir("")))
+	http.Handle("/public/assets/images/", http.FileServer(http.Dir(workingDirectory)))
+	http.Handle("/public/javascript/", http.FileServer(http.Dir(workingDirectory)))
+	http.Handle("/public/stylesheets/", http.FileServer(http.Dir(workingDirectory)))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
